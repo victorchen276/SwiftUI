52429e2b27814c2eb86549b8ec28dbb021abac94
Victor Chen <victor.chen@spark.co.nz>
2022-03-13 17:25:49 +1300

    add files

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chen Yue on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
