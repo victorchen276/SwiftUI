e82470b180ec912bd61414a1951a9e8b3b09ac6e
Victor Chen <victor.chen@spark.co.nz>
2022-03-13 22:01:38 +1300

    add fetch data from sever

--- ApiCall/ApiCall/ContentView.swift
+++ ApiCall/ApiCall/ContentView.swift
@@ -7,13 +7,103 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Course: Hashable, Codable {
+    
+    enum CodingKeys: String, CodingKey {
+            case name
+            case imageLink = "image"
+        }
+    
+    let name: String
+    let imageLink: String
+    
+}
+
+
+class ViewModel: ObservableObject {
+    
+    @Published var courses: [Course] = []
+    
+    func fetch() {
+        guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+}
+
+struct URLImage: View {
+    
+    let urlString: String
+    @State var data: Data?
+    
     var body: some View {
-        
+        if let data = data, let uiImage = UIImage(data: data){
+            Image(uiImage: uiImage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+        }
+        else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear {
+                    fetchData()
+                }
+        }
+    }
+    
+    private func fetchData() {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+ 
+}
+
+struct ContentView: View {
+
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(viewModel.courses, id: \.self) { course in
+                    HStack {
+                        URLImage(urlString: course.imageLink)
+                        Text(course.name).bold()
+                    }
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
+        }
         
         Text("Hello, world!")
             .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
